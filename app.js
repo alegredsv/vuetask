85850accf9337b62c08002be2e3507e892afddae
Adriano Wichmann <estudoalegre@gmail.com>
2016-08-12 13:47:23 -0300

    - Cria app

--- app.js
+++ app.js
@@ -25,13 +25,13 @@ var app = new Vue({
             'Empréstimo',
             'Gasolina'
         ],
-        bills:[{date_due:'20/08/2016', name: 'Conta de energia', value:127.99,done:1},
-       {date_due:'21/08/2016', name: 'Conta de águra', value:40.99,done:0},
+        bills:[{date_due:'20/08/2016', name: 'Conta de luz', value:127.99,done:1},
+       {date_due:'21/08/2016', name: 'Conta de água', value:40.99,done:0},
        {date_due:'22/08/2016', name: 'Conta de telefone', value:55.99,done:0},
-       {date_due:'23/08/2016', name: 'Supermenrcado', value:625.99,done:0},
+       {date_due:'23/08/2016', name: 'Supermercado', value:625.99,done:0},
        {date_due:'24/08/2016', name: 'Cartão de crédito', value:1200.99,done:0},
-       {date_due:'25/08/2016', name: 'Alimentação', value:25.99,done:1},
-       {date_due:'26/08/2016', name: 'Conta de condomínio', value:155.99,done:0}]
+       {date_due:'25/08/2016', name: 'Empréstimo', value:25.99,done:1},
+       {date_due:'26/08/2016', name: 'Gasolina', value:155.99,done:0}]
     },
     computed:{
         status: function () {
@@ -42,7 +42,7 @@ var app = new Vue({
                 }
             }
 
-            return !count?'Nenhuma conta a pagar':'Existem '+count+ ' linhas a pagar';
+            return !count?'Nenhuma conta a pagar':'Existem '+count+ ' contas a pagar';
         }
     },
     methods:{
@@ -54,10 +54,22 @@ var app = new Vue({
         submit:function () {
             this.bills.push(this.bill);
             this.activedView =0;
+        },
+        editaConta:function (bill) {
+            console.log(bill);
+            this.bill = bill;
+            this.activedView = 1;
         }
     }
 });
 
 app.$watch('teste',function (novoValor, velhoValor) {
     console.log(velhoValor+" : "+novoValor);
+});
+Vue.filter("doneLabel", function (value) {
+    if(value == 0){
+        return 'Não paga';
+    }else{
+        return 'Paga';
+    }
 });
