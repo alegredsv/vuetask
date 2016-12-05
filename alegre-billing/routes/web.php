ca483b4d247db39cac635eb9d3fed61c0b2f9577
Adriano Wichmann <estudoalegre@gmail.com>
2016-12-04 22:34:45 -0200

    Integrando webpack

--- alegre-billing/routes/web.php
+++ alegre-billing/routes/web.php
@@ -11,17 +11,29 @@
 |
 */
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
 
+Route::get('/user', function () {
+   // Auth::loginUsingId(2);
+    Auth::logout();
+});
+
 Route::get('/', function () {
-//    if(Gate::allows('access-admin')){
-//        return "Usuario com permissao de admin";
-//    }else{
-//        return "Usuario sem permissao de admin";
-//    }
+
     return view('welcome');
-})->middleware('can:access-admin');
+});
 
-Auth::routes();
 
-Route::get('/home', 'HomeController@index');
+
+Route::get('/home', function () {
+   return redirect()->route('admin.home');
+});
+
+Route::group(['prefix' => 'admin', 'as' =>'admin.' ],function (){
+    Auth::routes();
+    Route::group(['middleware' =>'can:access-admin'],function (){
+        Route::get('/home', 'HomeController@index')->name('home');
+    });
+
+});
